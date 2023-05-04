e2ba68ca6db6e2b3d6a46fb12d417549ea791007
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-05-04 19:36:47 +0200

    FEAT Milestone 3 and 4

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Project from './components/Project.vue';
+
+export default {
+  components: {
+    Project,
+  },
+}
+</script>
+
+<template>
+  <div class="container">
+    <Project></Project>
+  </div>
+</template>
+
+<style >
+
+</style>
---
+++ src/components/Project.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p v-for="project in projects" :key="project.id"> {{ project.title }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+    export default {
+        data() {
+            return {
+                projects: []
+            }
+        },
+        methods: {
+            fetchProject() {
+                axios.get('http://127.0.0.1:8000/api/projects')
+                .then(res => {
+                    console.log(res)
+                    const { results } = res.data
+                    this.projects = results
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+            }
+        }, 
+        mounted () {
+            this.fetchProject();
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
